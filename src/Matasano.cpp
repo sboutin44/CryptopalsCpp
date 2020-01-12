3d82c31a702fea8d0f11dbe24c67dd548477aea6
Sebastien Boutin <sboutin44@me.com>
2020-01-12 01:23:02 +0100

    Added more tests.

--- src/Matasano.cpp
+++ src/Matasano.cpp
@@ -105,43 +105,72 @@ int main() {
     // Sources of the inputs:
     // https://en.wikipedia.org/wiki/Base64
 
+    // Declarations like 
     char* res1;
     char* res2;
     char* res3;
     char* res4;
     char* res5;
-    int size;
-    const char* sebastien = "Sebastien";
+    char* res6;
+    char* res7;
+    char* res8;
+    char* res9;
+    char* res10;
+
     const char* input_1 = "any carnal pleasure.";
     const char* input_2 = "any carnal pleasure";
     const char* input_3 = "any carnal pleasur";
     const char* input_4 = "any carnal pleasu";
     const char* input_5 = "any carnal pleas";
-
-    const char hexString[] = {0x49,0x27,0x6d,0x20,0x6b,0x69,0x6c,0x6c,0x69,0x6e,0x67,0x20,0x79,0x6f,0x75,0x72,0x20,0x62,0x72,0x61,0x69,0x6e,0x20,0x6c,0x69,0x6b,0x65,0x20,0x61,0x20,0x70,0x6f,0x69,0x73,0x6f,0x6e,0x6f,0x75,0x73,0x20,0x6d,0x75,0x73,0x68,0x72,0x6f,0x6f,0x6d};
+    const char* input_6 = "pleasure.";
+    const char* input_7 = "leasure.";
+    const char* input_8 = "easure.";
+    const char* input_9 = "asure.";
+    const char* input_10 = "sure.";
 
     const char* expected_output_1 = "YW55IGNhcm5hbCBwbGVhc3VyZS4=";
     const char* expected_output_2 = "YW55IGNhcm5hbCBwbGVhc3VyZQ==";
     const char* expected_output_3 = "YW55IGNhcm5hbCBwbGVhc3Vy";
     const char* expected_output_4 = "YW55IGNhcm5hbCBwbGVhc3U=";
     const char* expected_output_5 = "YW55IGNhcm5hbCBwbGVhcw==";
-
-    size = getLength(input_1);
-    res1 = base64Encode(input_1,size);
+    const char* expected_output_6 = "cGxlYXN1cmUu";
+    const char* expected_output_7 = "bGVhc3VyZS4=";
+    const char* expected_output_8 = "ZWFzdXJlLg==";
+    const char* expected_output_9 = "YXN1cmUu";
+    const char* expected_output_10 = "c3VyZS4=";
+    
+    const char challenge[] = {0x49,0x27,0x6d,0x20,0x6b,0x69,0x6c,0x6c,0x69,0x6e,0x67,0x20,0x79,0x6f,0x75,0x72,0x20,0x62,0x72,0x61,0x69,0x6e,0x20,0x6c,0x69,0x6b,0x65,0x20,0x61,0x20,0x70,0x6f,0x69,0x73,0x6f,0x6e,0x6f,0x75,0x73,0x20,0x6d,0x75,0x73,0x68,0x72,0x6f,0x6f,0x6d};
+    int size_challenge = sizeof(challenge);
+    
+    // Tests 
+    res1 = base64Encode(input_1,getLength(input_1));
     res2 = base64Encode(input_2,getLength(input_2));
     res3 = base64Encode(input_3,getLength(input_3));
     res4 = base64Encode(input_4,getLength(input_4));
     res5 = base64Encode(input_5,getLength(input_5));
-
-    cout << res3 ;
+    res6 = base64Encode(input_6,getLength(input_6));
+    res7 = base64Encode(input_7,getLength(input_7));
+    res8 = base64Encode(input_8,getLength(input_8));
+    res9 = base64Encode(input_9,getLength(input_9));
+    res10 = base64Encode(input_10,getLength(input_10));
     
+    // Checks
     assert(strcmp(res1,expected_output_1) == 0);
     assert(strcmp(res2,expected_output_2) == 0);
     assert(strcmp(res3,expected_output_3) == 0);
     assert(strcmp(res4,expected_output_4) == 0);
     assert(strcmp(res5,expected_output_5) == 0);
 
+    assert(strcmp(res6,expected_output_6) == 0);
+    assert(strcmp(res7,expected_output_7) == 0);
+    assert(strcmp(res8,expected_output_8) == 0);
+    assert(strcmp(res9,expected_output_9) == 0);
+    assert(strcmp(res10,expected_output_10) == 0);
     
+
+    res1 = base64Encode(challenge,size_challenge);
+
+    cout << res1 << endl;
 
     return 0;
 }
