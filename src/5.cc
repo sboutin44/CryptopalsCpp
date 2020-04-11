7d8484f09d4907cc90a33efbafd7f0a636a93b29
Sebastien Boutin <sboutin44@me.com>
2020-04-11 14:41:49 -0700

    Added an assert in challenge 5.

--- src/5.cc
+++ src/5.cc
@@ -40,6 +40,13 @@ void challenge_5() {
   cout << "5. Implement repeating-key XOR" << endl;
   cout << "------------------------------------\n" << endl;
 
+  const char* expected_str =
+      "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765"
+      "272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27"
+      "282f";
+  uint8_t* expected = new uint8_t[strlen(expected_str) * 2];
+  hexDecode(expected_str, expected);
+
   const char* a =
       "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a "
       "cymbal";
@@ -49,5 +56,8 @@ void challenge_5() {
   output = new char[strlen(a)];
   repeatedKeyXor(a, key, output);
   for (int i = 0; i < (int)strlen(a); i++) printf("%02x", output[i]);
+
+  assert(memcmp((uint8_t*)output, expected, strlen(expected_str) * 2) == 0);
+
   delete[] output;
 }
