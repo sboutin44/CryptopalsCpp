908185aa5233d320e4bb848d43a5aff483c05f8f
Sebastien Boutin <sboutin44@me.com>
2020-04-11 17:23:56 -0700

    Bad length for allocated output array.

--- src/5.cc
+++ src/5.cc
@@ -44,7 +44,7 @@ void challenge_5() {
       "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765"
       "272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27"
       "282f";
-  uint8_t* expected = new uint8_t[strlen(expected_str) * 2];
+  uint8_t* expected = new uint8_t[strlen(expected_str) / 2];
   hexDecode(expected_str, expected);
 
   const char* a =
@@ -55,9 +55,13 @@ void challenge_5() {
 
   output = new char[strlen(a)];
   repeatedKeyXor(a, key, output);
+  printf("\n");
   for (int i = 0; i < (int)strlen(a); i++) printf("%02x", output[i]);
+  printf("\n");
+  for (int i = 0; i < (int)strlen(a); i++) printf("%02x", expected[i]);
+  printf("\n");
 
-  assert(memcmp((uint8_t*)output, expected, strlen(expected_str) * 2) == 0);
+  assert(memcmp((uint8_t*)output, expected, strlen(expected_str) / 2) == 0);
 
   delete[] output;
 }
