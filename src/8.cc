2cb23a48fe01d15e2d0b0d5fd29a3abcd7a7c59d
Sebastien Boutin <sboutin44@me.com>
2020-04-23 11:40:36 -0700

    Missing files.

---
+++ src/8.cc
@@ -0,0 +1,16 @@
+#include "aes.h"
+#include "lib.h"
+using namespace std;
+
+void challenge_8()
+{
+  cout << "\n------------------------------------" << endl;
+  cout << "Challenges Set 1" << endl;
+  cout << "8. Detect AES in ECB mode" << endl;
+  cout << "------------------------------------\n" << endl;
+
+  int len = 0;
+  byte* ciphertext = read_base64_file("resources/8.txt", &len);
+
+  delete[] ciphertext;
+}
